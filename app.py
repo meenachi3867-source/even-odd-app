a2195f467cf944046764306a3dfeae7896cedcc4
meenachi3867-source <meenachi3867@msec.edu.in>
2026-06-17 00:23:23 -0700

    Refactor even/odd check into a function

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-import os
+def check_even_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
 
-number = int(os.getenv("NUMBER", "0"))
 
-if number % 2 == 0:
-    print(f"{number} is Even")
-else:
-    print(f"{number} is Odd")
+if __name__ == "__main__":
+    num = int(input("Enter a number: "))
+    print(check_even_odd(num))
